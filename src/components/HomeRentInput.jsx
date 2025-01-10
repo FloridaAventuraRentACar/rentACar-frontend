28654eba02fcfd8a6c997bd22dd52b5501faf142
Felipe Del Zoppo <felipedelzoppo@gmail.com>
2025-01-10 10:49:01 -0700

    Centre horizontalmente los location selec

--- src/components/HomeRentInput.jsx
+++ src/components/HomeRentInput.jsx
@@ -25,39 +25,42 @@ export default function HomeRentInput() {
   return (
     <div className="form-container">
       <form onSubmit={handleSubmit} className="form">
-        <div className="form-group">
-          <label htmlFor="pickupLocation" className="form-label">
-            Pickup location
-          </label>
-          <select 
-            className='form-input'
-            id="pickupLocation" 
-            value={pickupLocation} 
-            onChange={(e) => setPickupLocation(e.target.value)}
-            required
-          >
-            <option id='default' value="" disabled selected>Select a pickup location</option>
-            <option value="Miami international airport">Miami international airport</option>
-            <option value="Fortlaurendale airport">Fortlaurendale airport</option>
-            <option value="Downtown">Downtown</option>
-          </select>
-        </div>
-        <div className="form-group">
-          <label htmlFor="returnLocation" className="form-label">
-            Return location
-          </label>
-          <select 
-            className='form-input'
-            id="returnLocation" 
-            value={returnLocation} 
-            onChange={(e) => setReturnLocation(e.target.value)}
-            required
-          >
-            <option id='default' value="" disabled selected>Select a return location</option>
-            <option value="Miami international airport">Miami international airport</option>
-            <option value="Fortlaurendale airport">Fortlaurendale airport</option>
-            <option value="Downtown">Downtown</option>
-          </select>
+        <div className="location">
+
+          <div className="form-group">
+            <label htmlFor="pickupLocation" className="form-label">
+              Pickup location
+            </label>
+            <select 
+              className='form-input'
+              id="pickupLocation" 
+              value={pickupLocation} 
+              onChange={(e) => setPickupLocation(e.target.value)}
+              required
+            >
+              <option id='default' value="" disabled selected>Select a pickup location</option>
+              <option value="Miami international airport">Miami international airport</option>
+              <option value="Fortlaurendale airport">Fortlaurendale airport</option>
+              <option value="Downtown">Downtown</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="returnLocation" className="form-label">
+              Return location
+            </label>
+            <select 
+              className='form-input'
+              id="returnLocation" 
+              value={returnLocation} 
+              onChange={(e) => setReturnLocation(e.target.value)}
+              required
+            >
+              <option id='default' value="" disabled selected>Select a return location</option>
+              <option value="Miami international airport">Miami international airport</option>
+              <option value="Fortlaurendale airport">Fortlaurendale airport</option>
+              <option value="Downtown">Downtown</option>
+            </select>
+          </div>
         </div>
         <div className="form-group">
           <label htmlFor="startDate" className="form-label">
