79b54a8845c865d41bc7462b052c578a3ccf5c42
Felipe Del Zoppo <felipedelzoppo@gmail.com>
2025-01-23 18:05:33 -0700

    Cree React router

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '../styles/homePage.css'
 import { HomePage } from './HomePage.jsx'
+import {  createBrowserRouter, RouterProvider} from 'react-router-dom';
+import {ShowCarsPage } from './ShowCarsPage.jsx'
+const router = createBrowserRouter([{
+  path: '/',
+  element: <HomePage />
+  },{
+    path: '/cars',
+    element: <ShowCarsPage />
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HomePage />
-  </StrictMode>,
+    <RouterProvider router={router} />;
+  </StrictMode>
 )
