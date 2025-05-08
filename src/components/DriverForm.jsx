075f9c641fe487ad345cb792a931c41ea63177ac
felipedelzoppo <felipedelzoppo@gmail.com>
2025-05-08 16:22:49 -0300

    Agregue checkbox mas 25 en DriverForm

--- src/components/DriverForm.jsx
+++ src/components/DriverForm.jsx
@@ -3,7 +3,8 @@ import { Formik, Form, Field, ErrorMessage, FieldArray } from 'formik';
 import * as Yup from 'yup';
 
 import styles from '../styles/DriverForm.module.css';
-
+import { Divider } from '@mui/material';
+import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
 const driverSchema = Yup.object({
   firstName: Yup.string().required('Required'),
   lastName: Yup.string().required('Required'),
@@ -14,6 +15,7 @@ const driverSchema = Yup.object({
   nameOnLicense: Yup.string().required('Required'),
   addressOnLicense: Yup.string().required('Required'),
   licenseExpiry: Yup.date().required('Required'),
+  ageCheckbox: Yup.boolean().oneOf([true]).required('Required')
 });
 
 const DriverForm = () => {
@@ -30,6 +32,7 @@ const DriverForm = () => {
       nameOnLicense: '',
       addressOnLicense: '',
       licenseExpiry: '',
+      ageCheckbox: false
     },
     additionalDrivers: [],
   };
@@ -42,120 +45,135 @@ const DriverForm = () => {
   const renderFields = (prefix) => (
     <>
       <div className={styles.fieldGroup}>
-        <label className={styles.label}>First Name:</label>
+        <label className={styles.label}>Nombre</label>
         <Field className={styles.input} name={`${prefix}.firstName`} />
         <ErrorMessage className={styles.error} name={`${prefix}.firstName`} component="div" />
       </div>
 
       <div className={styles.fieldGroup}>
-        <label className={styles.label}>Last Name:</label>
+        <label className={styles.label}>Apellido</label>
         <Field className={styles.input} name={`${prefix}.lastName`} />
         <ErrorMessage className={styles.error} name={`${prefix}.lastName`} component="div" />
       </div>
 
       <div className={styles.fieldGroup}>
-        <label className={styles.label}>Email:</label>
+        <label className={styles.label}>Email</label>
         <Field className={styles.input} type="email" name={`${prefix}.email`} />
         <ErrorMessage className={styles.error} name={`${prefix}.email`} component="div" />
       </div>
 
       <div className={styles.fieldGroup}>
-        <label className={styles.label}>Phone Number:</label>
+        <label className={styles.label}>Numero de telefono</label>
         <Field className={styles.input} name={`${prefix}.phone`} />
         <ErrorMessage className={styles.error} name={`${prefix}.phone`} component="div" />
       </div>
 
       <div className={styles.fieldGroup}>
-        <label className={styles.label}>Driver’s License Number:</label>
+        <label className={styles.label}>Numero de licencia de conducir</label>
         <Field className={styles.input} name={`${prefix}.licenseNumber`} />
         <ErrorMessage className={styles.error} name={`${prefix}.licenseNumber`} component="div" />
       </div>
 
       <div className={styles.fieldGroup}>
-        <label className={styles.label}>Date of Birth:</label>
+        <label className={styles.label}>Fecha de nacimiento</label>
         <Field className={styles.input} type="date" name={`${prefix}.birthDate`} />
         <ErrorMessage className={styles.error} name={`${prefix}.birthDate`} component="div" />
       </div>
 
       <div className={styles.fieldGroup}>
-        <label className={styles.label}>Name on License:</label>
+        <label className={styles.label}>Nombre en licencia de conducir</label>
         <Field className={styles.input} name={`${prefix}.nameOnLicense`} />
         <ErrorMessage className={styles.error} name={`${prefix}.nameOnLicense`} component="div" />
       </div>
 
       <div className={styles.fieldGroup}>
-        <label className={styles.label}>Address on License:</label>
+        <label className={styles.label}>Direccion en licencia de conducir</label>
         <Field className={styles.input} name={`${prefix}.addressOnLicense`} />
         <ErrorMessage className={styles.error} name={`${prefix}.addressOnLicense`} component="div" />
       </div>
 
       <div className={styles.fieldGroup}>
-        <label className={styles.label}>License Expiry Date:</label>
+        <label className={styles.label}>Fecha de expiracion de licencia de conducir</label>
         <Field className={styles.input} type="date" name={`${prefix}.licenseExpiry`} />
         <ErrorMessage className={styles.error} name={`${prefix}.licenseExpiry`} component="div" />
+      </div>
+      <div className={styles.checkbox}>
+        <Field type="checkbox" name={`${prefix}.ageCheckbox`} className={styles.ageCheckbox} />
+        <label className={styles.label}>
+          Tengo mas de 25 años
+        </label>
+        <ErrorMessage className={styles.error} name={`${prefix}.ageCheckbox`} component="div" />
       </div>
     </>
   );
 
   return (
-    <div className={styles.formContainer}>
-      <h2 className={styles.formTitle}>Driver Form</h2>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-      >
-        {({ values }) => (
-          <Form>
-            <h3 className={styles.sectionTitle}>Main Driver</h3>
-            {renderFields('driver')}
+    <div className={styles.container}>
+      <div className={styles.formContainer}>
+        <div className={styles.header}>
+          <button className={styles.backButton}>
+            <ArrowCircleLeftIcon className={styles.backIcon} />
+          </button>
+          <h2 className={styles.formTitle}>Volver a los detalles de la reserva</h2>
+        </div>
+        <Divider />
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+        >
+          {({ values }) => (
+            <Form>
+              <h3 className={styles.sectionTitle}>Conductor principal</h3>
+              {renderFields('driver')}
 
-            <h3 className={styles.sectionTitle}>Additional Drivers</h3>
-            <FieldArray name="additionalDrivers">
-              {({ push, remove }) => (
-                <div>
-                  {values.additionalDrivers.map((_, index) => (
-                    <div key={index} className={styles.driverBox}>
-                      <h4 className={styles.sectionTitle}>Additional Driver {index + 1}</h4>
-                      {renderFields(`additionalDrivers[${index}]`)}
-                      <button
-                        type="button"
-                        className={styles.formButton}
-                        onClick={() => remove(index)}
-                      >
-                        Remove
-                      </button>
-                    </div>
-                  ))}
-                  <button
-                    type="button"
-                    className={styles.formButton}
-                    onClick={() =>
-                      push({
-                        firstName: '',
-                        lastName: '',
-                        email: '',
-                        phone: '',
-                        licenseNumber: '',
-                        birthDate: '',
-                        nameOnLicense: '',
-                        addressOnLicense: '',
-                        licenseExpiry: '',
-                      })
-                    }
-                  >
-                    Add Additional Driver
-                  </button>
-                </div>
-              )}
-            </FieldArray>
+              <h3 className={styles.sectionTitle}>Conductores adicionales</h3>
+              <FieldArray name="additionalDrivers">
+                {({ push, remove }) => (
+                  <div>
+                    {values.additionalDrivers.map((_, index) => (
+                      <div key={index} className={styles.driverBox}>
+                        <h4 className={styles.sectionTitle}>Conductor adicional {index + 1}</h4>
+                        {renderFields(`additionalDrivers[${index}]`)}
+                        <button
+                          type="button"
+                          className={styles.formButton}
+                          onClick={() => remove(index)}
+                        >
+                          Remover
+                        </button>
+                      </div>
+                    ))}
+                    <button
+                      type="button"
+                      className={styles.formButton}
+                      onClick={() =>
+                        push({
+                          firstName: '',
+                          lastName: '',
+                          email: '',
+                          phone: '',
+                          licenseNumber: '',
+                          birthDate: '',
+                          nameOnLicense: '',
+                          addressOnLicense: '',
+                          licenseExpiry: '',
+                        })
+                      }
+                    >
+                      Agregar conductor adicional
+                    </button>
+                  </div>
+                )}
+              </FieldArray>
 
-            <br />
-            <button type="submit" className={styles.submitButton}>
-              Submit
-            </button>
-          </Form>
-        )}
-      </Formik>
+              <br />
+              <button type="submit" className={styles.submitButton}>
+                Confirmar reserva
+              </button>
+            </Form>
+          )}
+        </Formik>
+      </div>
     </div>
   );
 };
