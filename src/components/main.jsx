65fd65381340b1f08853bdadabe6ab7190517c3e
felipedelzoppo <felipedelzoppo@gmail.com>
2025-05-16 12:45:51 -0300

    Agregue ScrollToTop

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -7,20 +7,19 @@ import {ShowCarsPage } from './ShowCarsPage.jsx'
 import { CarRentalPage } from './CarRentalPage.jsx'
 import { AppProvider } from '../context/AppContext.jsx';
 import DriverForm from './DriverForm.jsx';
+import Layout  from './Layout.jsx';
 
-const router = createBrowserRouter([{
-  path: '/',
-  element: <HomePage />
-  },{
-    path: '/cars',
-    element: <ShowCarsPage />
-  },{
-    path: '/cars/:name',
-    element: <CarRentalPage />
-  },{
-    path: '/driver-form',
-    element: <DriverForm />
-  }
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/cars", element: <ShowCarsPage /> },
+      { path: "/cars/:name", element: <CarRentalPage /> },
+      { path: "/driver-form", element: <DriverForm /> },
+    ],
+  },
 ]);
 
 createRoot(document.getElementById('root')).render(
