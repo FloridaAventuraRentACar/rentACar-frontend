f3c56a41f6e53356dfdb4d67fd3fd4548dd20df0
felipedelzoppo <felipedelzoppo@gmail.com>
2025-07-14 20:08:52 -0300

    feat: Implemente JWT en frontend. Proteji rutas de administrador. #37

--- src/components/admin/Login.jsx
+++ src/components/admin/Login.jsx
@@ -1,35 +1,50 @@
-
-import { useState } from "react"
-import styles from "../../styles/admin/Login.module.css"
+import { useContext, useState } from "react";
+import styles from "../../styles/admin/Login.module.css";
+import fetchLogin from "../../services/userService";
+import { AuthContext } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
   const [formData, setFormData] = useState({
     email: "",
     password: "",
-  })
+  });
+  const { login } = useContext(AuthContext);
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setFormData({
       ...formData,
       [e.target.name]: e.target.value,
-    })
-  }
+    });
+  };
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log("Login attempt:", formData)
-    // Handle login logic here
-  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("Login attempt:", formData);
+    try {
+      const response = await fetchLogin(formData.email, formData.password);
+      const data = await response.data;
+      login(data.jwt);
+      console.log(data);
+      navigate("/admin");
+    } catch (error) {
+      console.log(error);
+      alert("Email o contraseña incorrectos");
+    }
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.loginCard}>
         <div className={styles.header}>
           <h1 className={styles.title}>Bienvenido de vuelta</h1>
-          <p className={styles.subtitle}>Por favor inicia sesion como administrador</p>
+          <p className={styles.subtitle}>
+            Por favor inicia sesion como administrador
+          </p>
         </div>
 
-        <form className={styles.form} onSubmit={handleSubmit}>
+        <form className={styles.form} onSubmit={handleSubmit} method="POST">
           <div className={styles.inputGroup}>
             <label htmlFor="email" className={styles.label}>
               Correo electronico
@@ -38,6 +53,7 @@ export default function Login() {
               type="email"
               id="email"
               name="email"
+              autoComplete="username"
               className={styles.input}
               placeholder="Ingresa tu correo"
               value={formData.email}
@@ -54,6 +70,7 @@ export default function Login() {
               type="password"
               id="password"
               name="password"
+              autoComplete="current-password"
               className={styles.input}
               placeholder="Ingresa tu contraseña"
               value={formData.password}
@@ -68,5 +85,5 @@ export default function Login() {
         </form>
       </div>
     </div>
-  )
+  );
 }
