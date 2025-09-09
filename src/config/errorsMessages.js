647e427d746a3369f2d3de6f19bebec88e98028a
Felipe Del Zoppo <felipedelzoppo@gmail.com>
2025-09-09 18:50:35 -0300

    feat: Cree objeto de errores

---
+++ src/config/errorsMessages.js
@@ -0,0 +1,8 @@
+export const errorsMessages = {
+    es:{
+        CAR_NOT_AVAILABLE: "El vehiculo no esta disponible en esas fechas"
+    },
+    en: {
+        CAR_NOT_AVAILABLE: "The car is not available in those dates"
+    }
+}
