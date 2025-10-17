ea158bad444def4bf281c57827eff0ecc0450ffc
felipedelzoppo <felipedelzoppo@gmail.com>
2025-10-17 10:46:23 -0300

    feat: Implete axiosInstance para envio de header Auth y apiKey en las peticiones al back. #57

---
+++ src/services/axiosInstance.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL || '', 
+});
+
+// Interceptor para agregar el token y la apiKey automáticamente
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  config.headers.apiKey = import.meta.env.VITE_API_KEY;
+  return config;
+});
+
+export default axiosInstance;
--- src/services/rentalService.js
+++ src/services/rentalService.js
@@ -1,42 +1,26 @@
-import axios from 'axios';
+import axiosInstance from './axiosInstance';
 import { API_GET_AVAILABILITY, API_GET_CURRENT_RENTALS, API_RENTALS } from '../config/apiConfig';
 
 export const getAvailability = (startDateTime, endDateTime) => {
-    return axios.get(`${API_GET_AVAILABILITY}?startDateTime=${startDateTime}&endDateTime=${endDateTime}`);
+    return axiosInstance.get(`${API_GET_AVAILABILITY}?startDateTime=${startDateTime}&endDateTime=${endDateTime}`);
 };
 
 export const postRental = (rental) => {
-    return axios.post(`${API_RENTALS}`, rental);
+    return axiosInstance.post(`${API_RENTALS}`, rental);
 };
 
 export const getCurrentRentals = () => {
-    return axios.get(`${API_GET_CURRENT_RENTALS}`, {
-        headers: {
-            "Authorization": `Bearer ${localStorage.getItem('token')}`
-        }
-    });
+    return axiosInstance.get(`${API_GET_CURRENT_RENTALS}`);
 };
 
 export const getRentalById = (rentalId) => {
-    return axios.get(`${API_RENTALS}/${rentalId}`,{
-        headers: {
-            "Authorization": `Bearer ${localStorage.getItem('token')}`
-        }
-    });
+    return axiosInstance.get(`${API_RENTALS}/${rentalId}`);
 };
 
 export const deleteRentalById = (rentalId) => {
-    return axios.delete(`${API_RENTALS}/${rentalId}`,{
-        headers: {
-            "Authorization": `Bearer ${localStorage.getItem('token')}`
-        }
-    });
+    return axiosInstance.delete(`${API_RENTALS}/${rentalId}`);
 };
 
 export const updateRental = (rental) => {
-    return axios.put(`${API_RENTALS}/${rental.id}`, rental, {
-        headers: {
-            "Authorization": `Bearer ${localStorage.getItem('token')}`
-        }
-    });
+    return axiosInstance.put(`${API_RENTALS}/${rental.id}`, rental);
 };
